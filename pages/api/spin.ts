913d1b7d0d86d218c87531cd6bb25b2f81f8d9a5
AnasAli30 <anasali12665@gmail.com>
2025-05-13 22:40:56 +0530

    added buy button

--- pages/api/spin.ts
+++ pages/api/spin.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../lib/mongo';
 
-const SPINS_PER_DAY = 3;
+const SPINS_PER_DAY = 2;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end();
