c89b51006296bdfa3a12350d06e8e5119c185d7d
AnasAli30 <anasali12665@gmail.com>
2025-05-16 10:43:05 +0530

    fixed

--- components/Home/EnvelopeReward.tsx
+++ components/Home/EnvelopeReward.tsx
@@ -32,7 +32,7 @@ export function EnvelopeReward({ setClaimed }: EnvelopeRewardProps) {
 
   const openEnvelope = async () => {
     setIsOpening(true);
-    const amount = +(Math.random() * (0.1 - 0.08) + 0.1).toFixed(4);
+    const amount = +(Math.random() * (0.05 - 0.03) + 0.01).toFixed(4);
     setReward(amount);
     const res = await fetchWithVerification("/api/send-envelope", {
       method: "POST",
