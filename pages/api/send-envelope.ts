d91d5f35904c7b2b39044f79479343a4567f9184
AnasAli30 <anasali12665@gmail.com>
2025-06-26 22:55:24 +0530

    .

--- pages/api/send-envelope.ts
+++ pages/api/send-envelope.ts
@@ -28,6 +28,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     await users.updateOne({ fid }, { $set: { envelopeClaimed: true, name } });
     res.status(200).json({ txHash: tx.hash });
   } catch (e: any) {
+    console.log(e.message)
     res.status(500).json({ error: e.message });
   }
 }
