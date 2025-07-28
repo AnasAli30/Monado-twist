fb75f512b61f23541999dc467d66046393b0e132
AnasAli30 <anasali12665@gmail.com>
2025-07-29 02:30:30 +0530

    change og-image

--- pages/api/og-image.ts
+++ pages/api/og-image.ts
@@ -103,7 +103,7 @@ export default async function handler(req: Request) {
           style: {
             position: 'absolute',
             bottom: 200,
-            right: 400,
+            right: 500,
             borderRadius: '50%',
             border: '4px solid #fff',
             zIndex: 2,
