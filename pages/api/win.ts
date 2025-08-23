055ae47d0254ed7d8e518d827e9817448112cce6
AnasAli30 <anasali12665@gmail.com>
2025-08-23 16:48:18 +0530

    .

--- pages/api/win.ts
+++ pages/api/win.ts
@@ -233,7 +233,7 @@ console.log("Forbidden",cleanIP)
     // Validate amounts with strict limits
     if (amount > 0.001 || amount <= 0) {
       console.log("Invalid amount",amount)
-      return res.status(400).json({ error: 'Invalid amount' });
+      return res.status(400).json({ error: 'fuck u bitch' });
     }
 
     // Connect to database
