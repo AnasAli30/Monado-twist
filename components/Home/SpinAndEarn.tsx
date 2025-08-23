cb61dbd02fab7085bb182f021b368f89cc57cf5c
AnasAli30 <anasali12665@gmail.com>
2025-08-23 19:46:06 +0530

    done

--- components/Home/SpinAndEarn.tsx
+++ components/Home/SpinAndEarn.tsx
@@ -1118,7 +1118,7 @@ Spin the wheel, touch grass later — it’s addictive af 🎰
                 Welcome to Monad TWIST! 🎰
               </h3>
               <p style={{ margin: '0 0 20px 0', fontSize: '1.1rem', lineHeight: '1.5' }}>
-                You can earn up to <strong style={{ color: '#FFD700' }}>50<img src="images/USDC.png" alt="" style={{ width: '40px', height: '40px',display:"inline-block"}} /> $USDC</strong> by just playing the game!
+                You can earn up to <strong style={{ color: '#FFD700' }}>50<img src="images/usdc.png" alt="" style={{ width: '40px', height: '40px',display:"inline-block"}} /> $USDC</strong> by just playing the game!
               </p>
             </div>
             <div className="popup-actions">
