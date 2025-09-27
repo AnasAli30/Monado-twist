565a68f66b0773e4dc39efc29cc28561c1c2c0d6
AnasAli30 <anasali12665@gmail.com>
2025-09-27 18:33:31 +0530

    done

--- components/Home/SpinAndEarn.tsx
+++ components/Home/SpinAndEarn.tsx
@@ -283,7 +283,7 @@ export function SpinAndEarn() {
   const getRandomValue = (token: string): number => {
     switch (token) {
       case "MON":
-        const monValues = [0.01,0.008,0.009,0.02];
+        const monValues =[0.1,0.08,0.09,0.02];;
         return monValues[Math.floor(Math.random() * monValues.length)];
       case "YAKI":
         return +(Math.random() * (1 - 0.5) + 0.5).toFixed(4);
